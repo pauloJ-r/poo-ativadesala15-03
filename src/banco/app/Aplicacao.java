171243cbd4f6c3cae858682cae2f6dbd330d2439
pauloJ-r <p74586@gmail.com>
2024-03-15 11:20:10 -0300

    feat: criação de cliente

--- src/banco/app/Aplicacao.java
+++ src/banco/app/Aplicacao.java
@@ -2,16 +2,21 @@ package banco.app;
 
 import java.math.BigDecimal;
 
+import banco.logica.Cliente;
 import banco.logica.conta;
 public class Aplicacao {
      public static void main(String[] args) {
     	 conta c = new conta ("1234");
     	 conta c1 = new conta ("1235");
+    		Cliente cli = new Cliente("000", "Gustavo");
     	 
     	 c.depositar(new BigDecimal("100.5"));
     	 c.sacar(new BigDecimal("50"));
     	 c.transferir(new BigDecimal("25"), c1);
-    	 System.out.println(c);
-    	 System.out.println(c1);
+    	 
+    	 cli.getContas().add(c1);
+    	 cli.getContas().add(c);
+    	
+    	 System.out.println(cli);
      }
 }
---
+++ src/banco/logica/Cliente.java
@@ -0,0 +1,62 @@
+package banco.logica;
+
+import java.util.ArrayList;
+import java.util.Objects;
+public class Cliente {
+    private String cpf;
+    private String nome;
+    
+    
+    ArrayList<conta> contas = new ArrayList<>();
+
+
+	public Cliente(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+		
+	}
+
+
+	@Override
+	public String toString() {
+		return "Cliente [cpf=" + cpf + ", nome=" + nome + ", contas=" + contas + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+    public String getNome() {
+		return nome;
+	}
+    public String getCpf() {
+		return cpf;
+	}
+    public ArrayList<conta> getContas() {
+		return contas;
+	}
+    public void setNome(String nome) {
+		this.nome = nome;
+	}
+    public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+    public void setContas(ArrayList<conta> contas) {
+		this.contas = contas;
+	}
+    
+}
